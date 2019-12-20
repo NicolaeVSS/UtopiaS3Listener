252d4fc8c5c1767687272e5bd6e5da3c4d213b27
NicolaeVSS <nicolae.vasile@smoothstack.com>
2019-12-20 15:01:27 -0500

    added comments

--- src/index.js
+++ src/index.js
@@ -15,17 +15,22 @@ class Response{
 // For async functions, you return a response, error, or promise to the runtime instead of using callback
 // you can use return and throw to send a response or error
 module.exports.handler = async (event, context) => {
+  // DEBUG:
+  // console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
+
   const bucketName = event.Records[0].s3.bucket.name;
   const objectkey = event.Records[0].s3.object.key;
-  // get the entity type, the object key should be formatted [entity].csv i.e. "airport.csv"
+  // get the entity type, the object key should be formatted "[entity].csv" i.e. "airport.csv"
   const entityType = objectkey.split(".")[0];
   const entities = [];
-  const myfile = s3.getObject({Bucket: bucketName, Key: objectkey}).createReadStream();
 
+  // get the file and marshal the csv rows into JSON objects
+  const myfile = s3.getObject({Bucket: bucketName, Key: objectkey}).createReadStream();
   await csv().fromStream(myfile).subscribe((json) => {
     entities.push(json);
   });
 
+  // based on the entity type, make different HTTP requests
   switch (entityType){
     case "airport":
       console.log("airport POST")
@@ -42,6 +47,5 @@ module.exports.handler = async (event, context) => {
 
   console.log(entities);
 
-  // console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
   return new Response(204);
 }
